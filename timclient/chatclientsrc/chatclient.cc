1887e3e62a1c3e9fd5704b9986c64f813dcdfe4b
GHzbq <eric_qiushui@126.com>
2019-05-28 17:33:54 +0800

    update chatclient.cc

--- timclient/chatclientsrc/chatclient.cc
+++ timclient/chatclientsrc/chatclient.cc
@@ -14,7 +14,17 @@ static void Usage(std::string proc)
     std::cout << "Usage: " << proc << " serverip" << std::endl;
 }
 
-int main(int argc, char* argv[])
+static void Menu(int32_t &input)
+{
+    std::cout << "************************************" << std::endl;
+    std::cout << "**   1.Register         2.Login   **" << std::endl;
+    std::cout << "**                      3.Exit    **" << std::endl;
+    std::cout << "************************************" << std::endl;
+    std::cout << "Please select:> ";
+    std::cin >> input;
+}
+
+int main(int32_t argc, char* argv[])
 {
     if(argc != 2)
     {
@@ -25,17 +35,41 @@ int main(int argc, char* argv[])
     chatclient::ChatClient *cp = new chatclient::ChatClient(argv[1]);
     cp->InitChatClient();
 
-    bool ret_conn = cp->ConnectToServer();
-    if(ret_conn)
+    int32_t choice = 0;
+    while(1)
     {
-        std::cout << "connect success." << std::endl;
-        // 为了方便观察，我们写一个死循环
-        while(1){}
+        Menu(choice);
+        switch(choice)
+        {
+        case 1: // Register
+            cp->Register();
+            break;
+        case 2: // Login
+            if(cp->Login())
+            {
+                cp->Chat();
+            }
+            break;
+        case 3: // Exit
+            exit(0);
+            break;
+        default:
+            break;
+        }
     }
-    else
-    {
-        std::cout << "connect failed." << std::endl;
-    }
+
+
+    // bool ret_conn = cp->ConnectToServer();
+    // if(ret_conn)
+    // {
+    //     std::cout << "connect success." << std::endl;
+    //     // 为了方便观察，我们写一个死循环
+    //     while(1){}
+    // }
+    // else
+    // {
+    //     std::cout << "connect failed." << std::endl;
+    // }
 
     return 0;
 }
